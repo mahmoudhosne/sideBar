7e8e02bdc36a361e010860cc329750ea00093405
mahmoudhosne <86571817+mahmoudhosne@users.noreply.github.com>
2023-04-26 10:20:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('button');
+
+let nav = document.querySelector('nav');
+btn.addEventListener('click', function () {
+  nav.classList.toggle('active');
+  btn.classList.toggle('active');
+});
